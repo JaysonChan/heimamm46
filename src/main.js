fd2966a2b1d6faff4b30f7263b0021d2d34a2199
unknown <chenguanyu>
2020-03-22 09:40:45 +0800

    登录模块 - 分支切换

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//导入路由
+import Router from './router/index.js'
+// 导入element-ui
+import ElementUI from 'element-ui'
+//导入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 注册element-ui
+vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //挂载到Vue实例上 路由
+  Router
 }).$mount('#app')
